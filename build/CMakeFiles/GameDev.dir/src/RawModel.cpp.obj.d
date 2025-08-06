a2183104391b15dc96fdb2ba3e1fe4dc1a7e4702
Mamoon Ataur Rahman <mamoongo21@gmail.com>
2025-08-06 22:23:58 +0530

    Removed ignored folders from repo

--- build/CMakeFiles/GameDev.dir/src/RawModel.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/GameDev.dir/src/RawModel.cpp.obj: \
- C:\dev\GameDev\src\RawModel.cpp C:/dev/GameDev/include/RawModel.h
--- build/CMakeFiles/GameDev.dir/src/TexturedModel.cpp.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/GameDev.dir/src/TexturedModel.cpp.obj: \
- C:\dev\GameDev\src\TexturedModel.cpp \
- C:/dev/GameDev/include/TexturedModel.h C:/dev/GameDev/include/RawModel.h \
- C:/dev/GameDev/include/ModelTexture.h
